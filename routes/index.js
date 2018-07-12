baf1f21c68cee329c6f4b640bb2348471733b1f6
Idowu Daramola <idowu.daramola@gmail.com>
2018-07-12 05:43:47 +0100

    modified routes. forgot to define the root path earlier

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 
-
+router.get('/', addExpenseController.expenseForm)
 router.get('/add', addExpenseController.expenseForm); // should render  add expense form
 router.post('/save', addExpenseController.saveExpense);
 router.get('/list', addExpenseController.listExpense)
